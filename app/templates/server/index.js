08d95438df54be7bcb348d1a91d0faa533a46290
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-25 00:35:07 -0500

    feat: add json server

--- app/templates/server/index.js
+++ app/templates/server/index.js
@@ -1,17 +1,28 @@
 /* eslint-env node */
 
+// Uncomment this for json-server support
+// const jsonServer = require('json-server');
+// const jsonRouter = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
 
 module.exports = function (app) {
   // Log proxy requests
   const morgan  = require('morgan');
   app.use(morgan('dev'));
 
+  // Uncomment this for json-server support
+  // app.use(middlewares);
+  // app.use((req, res, next) => {
+  //   if (req.accepts('json') && !req.accepts('html')) {
+  //     jsonRouter(req, res, next);
+  //   } else {
+  //     next();
+  //   }
+  // });
+
   // Uncomment this for client-side routing
   // app.use((req, res, next) => {
-  //   const acceptHeaders = req.headers.accept || [];
-  //   const hasHTMLHeader = acceptHeaders.indexOf('text/html') !== -1;
-  //
-  //   if (hasHTMLHeader) {
+  //   if (req.accepts('html')) {
   //     req.serveUrl = '/index.html';
   //   }
   //
